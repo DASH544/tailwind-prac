ff73dd793f2edd98959ffe9e4d1d15e42d08320f
Dawar Shah <dawarshah544@gmail.com>
2025-01-21 20:01:10 +0530

    weather-io

--- brainwave-frontend/src/App.jsx
+++ brainwave-frontend/src/App.jsx
@@ -1,7 +1,11 @@
+import Layout from "./Layout"
+
 function App() {
 
   return (
-   
+   <>
+   <Layout></Layout>
+   </>
   )
 }
 
--- brainwave-frontend/src/components/Header/Header.jsx
+++ brainwave-frontend/src/components/Header/Header.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function Header() {
   return (
+  <nav>
     <div>
       
     </div>
+  </nav>
   )
 }
 
